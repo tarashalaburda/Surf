67f862ff4cd8de5971980a962c610197c5e74844
tarasHalaburda <tarashalaburda@gmail.com>
2020-04-22 21:23:18 +0300

    added slider travel section

--- app/js/main.js
+++ app/js/main.js
@@ -32,4 +32,11 @@ $(function () {
 
 	});
 
+	$('.travel__slider').slick({
+		infinite: true,
+		fade: true,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="Arrow right">',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow_right.svg" alt="Arrow right">',
+	});
+
 });
